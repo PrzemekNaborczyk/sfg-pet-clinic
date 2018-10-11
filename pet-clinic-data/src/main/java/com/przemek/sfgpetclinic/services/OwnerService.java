33b531788bc810c32d8c79a0c2002a5cb64c476b
Przemyslaw Naborczyk <przemyslaw.naborczyk@capgemini.com>
2018-10-11 14:42:53 +0200

    -Extracted common code to CrudService. Closes #11

--- pet-clinic-data/src/main/java/com/przemek/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/przemek/sfgpetclinic/services/OwnerService.java
@@ -2,13 +2,5 @@ package com.przemek.sfgpetclinic.services;
 
 import com.przemek.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
+public interface OwnerService extends CrudService<Owner, Long> {
 }
